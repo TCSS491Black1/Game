d42043e0c04bff0b534b3db70244af54a61746ce
Peter Iriarte <piriar@uw.edu>
2023-01-12 11:36:28 -0800

    formatting

--- animator.js
+++ animator.js
@@ -1,42 +1,40 @@
 class Animator {
-    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration,loop){
-        Object.assign(this,{spritesheet, xStart, yStart, width, height, frameCount, frameDuration,loop});
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, loop) {
+        Object.assign(this, { spritesheet, xStart, yStart, width, height, frameCount, frameDuration, loop });
 
-        this.elapsedTime =0;
+        this.elapsedTime = 0;
         this.totalTime = frameCount * frameDuration;
 
     };
 
-    drawFrame(tick, ctx, x , y){
-
+    drawFrame(tick, ctx, x, y) {
         this.elapsedTime += tick;
         const frame = this.currentFrame();
         if (this.isDone()) {
-            if (this.loop==1) {
+            if (this.loop == 1) {
                 this.elapsedTime -= this.totalTime;
             } else {
                 ctx.drawImage(this.spritesheet,
-                    this.xStart + this.width*(this.frameCount-1), this.yStart,
-                    this.width,this.height,
+                    this.xStart + this.width * (this.frameCount - 1), this.yStart,
+                    this.width, this.height,
                     x, y,
-                    this.width*2,this.height*2)
+                    this.width * 2, this.height * 2)
             }
-        }else {
+        } else {
             //console.log("animator given:", this.spritesheet);
-             ctx.drawImage(this.spritesheet,
-                this.xStart + this.width*frame, this.yStart,
-                this.width,this.height,
+            ctx.drawImage(this.spritesheet,
+                this.xStart + this.width * frame, this.yStart,
+                this.width, this.height,
                 x, y,
-                this.width*2,this.height*2)
+                this.width * 2, this.height * 2)
         }
     };
 
-    currentFrame(){
+    currentFrame() {
         return Math.floor(this.elapsedTime / this.frameDuration);
     };
 
-    isDone(){
+    isDone() {
         return (this.elapsedTime >= this.totalTime);
     };
-
 };
