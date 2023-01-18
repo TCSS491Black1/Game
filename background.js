958aa1a6b4213742d7138602f38cbe0f9dd45815
Peter Iriarte <piriar@uw.edu>
2023-01-17 23:24:47 -0800

    whitspace

--- background.js
+++ background.js
@@ -3,8 +3,6 @@ class Background{
         this.game = game;
         this.x = 0;
         this.y = 0;
-        
-
     }
 
     update(){
@@ -12,13 +10,10 @@ class Background{
     }
 
     draw(ctx){
-
         ctx.drawImage(ASSET_MANAGER.getAsset("./assets/Overworld_Level_1.png"), this.x-this.game.camera.x ,this.y, 1920, 768,);
         ctx.drawImage(ASSET_MANAGER.getAsset("./assets/Overworld_Level_1.png"), this.x+1918-this.game.camera.x, this.y, 1920, 768,);
         ctx.drawImage(ASSET_MANAGER.getAsset("./assets/Overworld_Level_1.png"), this.x+(2*1918)-this.game.camera.x, this.y, 1920, 768,);
         ctx.drawImage(ASSET_MANAGER.getAsset("./assets/Overworld_Level_1.png"), this.x+(3*1918)-this.game.camera.x, this.y, 1920, 768,);
         ctx.drawImage(ASSET_MANAGER.getAsset("./assets/Overworld_Level_1.png"), this.x+(4*1918)-this.game.camera.x, this.y, 1920, 768,); // loooong background
-
-        
     }
 }
