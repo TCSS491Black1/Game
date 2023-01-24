91342db00716239c58cc0dc676d787d5c65be51e
Peter Iriarte <piriar@uw.edu>
2023-01-23 22:23:12 -0800

    basic flat level w/ platform

--- levels.js
+++ levels.js
@@ -1,6 +1,10 @@
 var levelOne = {
     music: "./assets/sounds/music/intro.mp3",
     background: "./assets/Overworld_Level_1.png",
-    ground: {x: 800, y: 600, size: 64},
+    ground: [
+        {x:0, y:params.canvasHeight-32, size: 32 * 736 },
+        {x:32*10, y:params.canvasHeight-32*5, size: 32 * 736 },
+        {x:32*20, y:params.canvasHeight-32*5, size: 32 * 73 },
+    ],
     targetblock: {x: 120, y: 40, size: 1},
 }
