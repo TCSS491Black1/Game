e4d2dc9b52f8921aa45acf8cab2abf56aac5f883
Peter Iriarte <piriar@uw.edu>
2023-01-23 22:20:27 -0800

    generalized "Ground" to be used as platforms

--- blocks.js
+++ blocks.js
@@ -1,16 +1,12 @@
 class Ground {
     constructor(game, x, y, w) {
         Object.assign(this, { game, x, y, w});
-
         this.spritesheet = ASSET_MANAGER.getAsset("./assets/Environmental_Blocks.png",0,0,16,16);
-
-
-        this.BB = new BoundingBox(0, params.canvasHeight-32, 32*w, 32);
-
+        this.BB = new BoundingBox(this.x, this.y, 32*w, 32, "orange");
     };
 
     update() {
-    
+        this.BB = new BoundingBox(this.x - this.game.camera.x, this.y, 32*this.w, 32, "orange");
     };
 
     draw(ctx){
@@ -18,10 +14,11 @@ class Ground {
             ctx.drawImage(this.spritesheet,
                 0, 0,
                 16,16,
-                i*32-this.game.camera.x, params.canvasHeight-32,
+                /*i*32-this.game.camera.x, params.canvasHeight-32,*/
+                this.x + i*32-this.game.camera.x, this.y,
                 16*2,16*2);
+                
         }
-        ctx.strokeStyle = 'Red';
-        ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
+        this.BB.draw(ctx);
     };
 }
