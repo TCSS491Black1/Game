8113aa158f476cbb8dc369ccbf068a9451ff5965
griffinryan <glryan@uw.edu>
2023-01-28 20:30:37 -0800

    bg music loops and figuring where put target

--- main.js
+++ main.js
@@ -21,11 +21,24 @@ const ASSET_MANAGER = new AssetManager();
 	"./assets/sounds/music/intro.mp3", // music, sound assets
 	"./assets/sounds/music/main-1.mp3",
 	"./assets/sounds/music/main-2.mp3",
+	"./assets/sounds/music/ver-1.mp3",
+	"./assets/sounds/music/ver-2.mp3",
+	"./assets/sounds/music/ver-3.mp3",
+	"./assets/sounds/sfx/step.wav",
+	"./assets/sounds/sfx/coin.wav",
+	"./assets/sounds/sfx/laser.wav",
+	"./assets/sounds/sfx/snare.wav",
+	"./assets/sounds/sfx/wall.wav",
 	"./assets/sounds/sfx/trill.wav",
 ].forEach(asset => ASSET_MANAGER.queueDownload(asset));
 
 ASSET_MANAGER.downloadAll(() => {
 	ASSET_MANAGER.autoRepeat("./assets/sounds/music/intro.mp3");
+	ASSET_MANAGER.autoRepeat("./assets/sounds/music/main-1.mp3");
+	ASSET_MANAGER.autoRepeat("./assets/sounds/music/main-2.mp3");
+	ASSET_MANAGER.autoRepeat("./assets/sounds/music/ver-1.mp3");
+	ASSET_MANAGER.autoRepeat("./assets/sounds/music/ver-2.mp3");
+	ASSET_MANAGER.autoRepeat("./assets/sounds/music/ver-3.mp3");
 	var canvas = document.getElementById("gameWorld");
 	var ctx = canvas.getContext("2d");
 	
