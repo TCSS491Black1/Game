7d12a6ddc6ec90f16b0d1aab943aec00bc9ad256
Peter Iriarte <piriar@uw.edu>
2023-02-12 03:58:34 -0800

    formatting

--- background.js
+++ background.js
@@ -5,8 +5,8 @@ class ParallaxLayer {
         this.speed = speed;
         this.yOffset = yOffset;
         this.game = gameEngine;
-        this.images = [];
 
+        this.images = [];
         for (const image of img) {
             this.images.push(ASSET_MANAGER.getAsset(image));
         }
@@ -37,28 +37,19 @@ class ParallaxLayer {
 class Background extends ParallaxLayer {
     constructor(game, img) {
         super(img, 0.8, 0);
-        this.x = 0;
-        this.y = 0;
-        this.game = game;
-        this.images=[];
-        //Loop down the background images and make them into an array of assets
-        for( var j=0 ; j < img.length;j++ ){
-            this.images[j] = ASSET_MANAGER.getAsset(img[j]);
-        }
-
     }
 
     draw(ctx) { // Backgrounds tile differently, so it needs it's own draw()
         //Starting from top background image drawn right then down one layer at a time
         // j for vertical position
         // i for horizontal position 
-        for( var j=0 ; j < this.images.length;j++ ){
-            let image =this.images[j];
+        for (var j = 0; j < this.images.length; j++) {
+            let image = this.images[j];
             for (var i = 0; i < 7; i++) {
                 ctx.drawImage(
                     image,
                     this.x + (i * 1920) - this.game.camera.x,
-                    this.y+(768*j)-this.game.camera.y,
+                    this.y + (768 * j) - this.game.camera.y,
                     1920, 768
                 );
 
