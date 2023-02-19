56b8242b5e00f37c47fa883b411a52f8459247b5
Peter Iriarte <piriar@uw.edu>
2023-02-19 01:13:06 -0800

    fixed bugs w/ attack mechanic

--- characterController.js
+++ characterController.js
@@ -36,7 +36,7 @@ class CharacterController {
         this.animationList["WALK"] = new Animator(spritesheet, 4, 1191, 159, 191, 8, 0.1, 1, 3, 0, 0, this.scale);
         this.animationList["JUMP"] = new Animator(spritesheet, 4, 1626, 188, 214, 9, 0.3, 0, 3, 0, 0, this.scale);
 
-        this.animationList["ATTACK"] = new Animator(this.attacksheet, 0, 0, 378, 371, 4, 0.1, 0, 0, 0, 100 * this.scale, this.scale);
+        this.animationList["ATTACK"] = new Animator(this.attacksheet, 0, 0, 378, 371, 4, 0.02, 0, 0, 0, 100 * this.scale, this.scale);
         
         this.animationList["DEATH"] = new Animator(spritesheet, 4, 9922, 300, 225, 5, 0.1, 0, 3, 0, -10, this.scale);
         this.animationList["DEAD"] = new Animator(spritesheet, 1216, 9922, 300, 225, 1, 0.5, 1, 3, 0, -10, this.scale);
@@ -140,8 +140,7 @@ class CharacterController {
                 }
             }
         } else if (attackTimeElapsed >= attackDuration) { // cleanup after attack
-            this.attackBeginTime = undefined;
-            this.animationList["ATTACK"] = new Animator(this.attacksheet, 0, 0, 378, 371, 4, 0.05, 0, 0, 0, 100 * this.scale, this.scale);
+            this.animationList["ATTACK"].reset();
             
             if (!this.onGround)
                 this.changeState("JUMP", 159);
